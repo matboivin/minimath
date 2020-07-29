3a54de10ae66e22b9b43eac5189bfa01336d6530
matboivin <mboivin@student.42.fr>
2020-07-29 20:08:59 +0200

    Remove math

--- includes/minimath.h
+++ includes/minimath.h
@@ -6,14 +6,13 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 21:32:52 by mboivin           #+#    #+#             */
-/*   Updated: 2020/07/29 02:23:45 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/07/29 20:08:29 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIMATH_H
 # define MINIMATH_H
 
-# include <math.h>
 # include <stdlib.h>
 
 # include "minimath_vec2.h"
